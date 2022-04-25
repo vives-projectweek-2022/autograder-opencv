40c2625b5ccc5a184b3a33bb1746c3b566728dde
T <deliltristan21@gmail.com>
2022-04-25 14:58:54 +0200

    pi

--- blobtest.py
+++ blobtest.py
@@ -1,99 +1,121 @@
-from unittest import skip
 import cv2
 import numpy as np
-import keyboard
 
-#read image
-cap = cv2.VideoCapture(0) 
-#correct answers
-correctAnswer = [2,4]
-circles = []
-amount = 0
-border = []
-#variables for circle blob
-params = cv2.SimpleBlobDetector_Params()
-params.filterByArea = True
-params.minArea = 300
-#filter by how Circular the circles are
-params.filterByCircularity = True
-params.minCircularity = 0.4
-#convexity
-params.filterByConvexity = True
-params.minConvexity = 0.2
-#threshhold
-params.thresholdStep = 5
-params.minThreshold = 200
-params.maxThreshold = 255
-#shape
-params.filterByInertia = True
-params.minInertiaRatio = 0.1
-#blob detector :D
-detector = cv2.SimpleBlobDetector_create(params)
+#draw function global variables
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+fontColor = (255,255,255)
+thickness = 1
+lineType  = 2
 
-#variables rectangles
-counter = 0
-parts = []
-areas = []
+#camera capture photo
+def camera():
+    #declare variables for camera detection
+    cap = cv2.VideoCapture(0)
+    borderbox = []
+    while(True):
+        _, frame = cap.read() 
+        #draw detected rectangles
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        canny = cv2.Canny(blurred, 50, 255, 1)
+        
+        rectangle = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        rectangle = rectangle[0] if len(rectangle) == 2 else rectangle[1]
 
-#draw
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-fontScale              = 1
-fontColor              = (255,255,255)
-thickness              = 1
-lineType               = 2
+        for rect in rectangle:
+            x,y,w,h = cv2.boundingRect(rect)
+            if((w) <= 35 and (w) >= 20 and (h) <= 35 and (h) >= 20):
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0,50,255), 1)
+                cv2.putText(frame, str(len(borderbox)) , (50,50), font, fontScale,fontColor,thickness,lineType)
+                if(len(borderbox) != 4):
+                    borderbox.append(rect)
 
-while(True):
-    _, frame = cap.read() 
-    #draw detected rectangles
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("camera frame",frame)
+        k = cv2.waitKey(5) & 0xFF
+        if(k == 27):#and len(borderbox) == 4
+            ROI = frame
+            return ROI
+            
+#area of intrest
+def answerArea(ROI):
+    #variables
+    borderX = []
+    borderY = []
+    #filter
+    gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
     canny = cv2.Canny(blurred, 50, 255, 1)
-    
+        
     rectangle = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     rectangle = rectangle[0] if len(rectangle) == 2 else rectangle[1]
 
     for rect in rectangle:
         x,y,w,h = cv2.boundingRect(rect)
-        if((x+w)-x <= 25 and (x+w)-x >= 10 and (y+h)- y <= 25 and (y+h)- y >= 10):
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255,50,0), 1)
-            
-
-    cv2.imshow("only black box",canny)    
-    cv2.imshow("Original Image",frame)
-    k = cv2.waitKey(5) & 0xFF
-    if(k == 27 and len(rectangle) == 4):
-        ROI = frame
-        break
-
-gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-canny = cv2.Canny(blurred, 50, 255, 1)
+        if((w) <= 35 and (w) >= 18 and (h) <= 35 and (h) >= 18):
+            cv2.rectangle(ROI, (x, y), (x + w, y + h), (255,50,255), 1)
+            Middel = cv2.moments(rect)
+            cX = int(Middel["m10"] / Middel["m00"])
+            cY = int(Middel["m01"] / Middel["m00"])
+            #cv2.circle(ROI, (cX, cY), 5, (255, 255, 255), -1)
+            #cv2.putText(ROI, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            if(len(borderX) != 4):
+                borderX.append(cX)
+                borderY.append(cY)
     
-rectangle = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-rectangle = rectangle[0] if len(rectangle) == 2 else rectangle[1]
+    #sort small to big
+    borderX.sort()
+    borderY.sort()
+    #show results
+    cv2.imshow("Image",ROI)
+    cv2.waitKey(0)
+    answer = ROI[borderY[0]:borderY[3],borderX[0]:borderX[3]]
+    cv2.imshow("answer Image",answer)
+    return answer 
 
-for rect in rectangle:
-    x,y,w,h = cv2.boundingRect(rect)
-    if((x+w)-x <= 25 and (x+w)-x >= 10 and (y+h)- y <= 25 and (y+h)- y >= 10):
-        cv2.rectangle(ROI, (x, y), (x + w, y + h), (0,50,255), 1)
-        if(len(border) != 8):
-                border.append(x)
-                border.append(y)
+#circle detecting 
+def cirlcedetecting(ROI):
+    #variables for circle blob
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = False
+    params.minArea = 0
+    params.maxArea = 120
+    #filter by how Circular the circles are
+    params.filterByCircularity = True
+    params.minCircularity = 0.2
+    #convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0.1
+    #threshhold
+    #params.thresholdStep = 5
+    #params.minThreshold = 120
+    #params.maxThreshold = 255
+    #shape
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.1
+    #blob detector :D
+    detector = cv2.SimpleBlobDetector_create(params)
 
-for i in range(0,len(border)):
-    print(border[i])
-cv2.putText(ROI, '1' , (border[0],border[1]), font, fontScale,fontColor,thickness,lineType)
-cv2.putText(ROI, '2' , (border[2],border[3]), font, fontScale,fontColor,thickness,lineType)
-cv2.putText(ROI, '3' , (border[4],border[5]), font, fontScale,fontColor,thickness,lineType)
-cv2.putText(ROI, '4' , (border[6],border[7]), font, fontScale,fontColor,thickness,lineType)
+    #convert frame
+    bilateral_filtered_image = cv2.bilateralFilter(ROI, 5, 120, 255)
+    edge_detected_image = cv2.Canny(bilateral_filtered_image, 50, 255)
+    cv2.imshow("Circular Blobs Only", bilateral_filtered_image)
+    cv2.imshow("Circular Blobs Only", edge_detected_image)
+    cv2.waitKey(0)
+    #detect circles
+    keypoints = detector.detect(edge_detected_image)
+    #print(str(keypoints[0].pt[0]))
+    #draw around blobs
+    blank = np.zeros((1, 1))
+    circle = cv2.drawKeypoints(ROI, keypoints, blank, (0, 0, 255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return circle
 
-#show results
-
-cv2.imshow("new Image",ROI)
+img = camera()
+area = answerArea(img)
+cv2.imshow("area", area)
 cv2.waitKey(0)
-img = ROI[border[5]:border[4],border[3]:border[2]]
-cv2.imshow("new Image",img) 
-#cv2.imshow("Circular Blobs Only", blobs)
+circles = cirlcedetecting(area)
+cv2.imshow("Circular Blobs Only", circles)
 #remove results
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- testwebcam.py
+++ testwebcam.py
@@ -4,42 +4,59 @@ import cv2
 import numpy as np 
   
 # Webcamera no 0 is used to capture the frames
-cap = cv2.VideoCapture(2) 
+cap = cv2.VideoCapture(0) 
 
-minDist = 100
-param1 = 30 #500
-param2 = 75 #200 #smaller value-> more false circles
-minRadius = 5
-maxRadius = 100 #10
+#variables for circle blob
+params = cv2.SimpleBlobDetector_Params()
+params.filterByArea = True
+params.minArea = 0
+#filter by how Circular the circles are
+params.filterByCircularity = True
+params.minCircularity = 0.4
+#params.
+params.filterByConvexity = True
+params.minConvexity = 0.2
+#threshhold
+params.thresholdStep = 5
+params.minThreshold = 0
+params.maxThreshold = 255
+
+params.filterByInertia = True
+params.minInertiaRatio = 0.1
+
+detector = cv2.SimpleBlobDetector_create(params)
 
 # This drives the program into an infinite loop.
 while(True):        
     # Captures the live stream frame-by-frame
     _, frame = cap.read() 
     # Converts images from BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    canny = cv2.Canny(blurred, 50, 255, 1)
+
+    rectangle = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rectangle = rectangle[0] if len(rectangle) == 2 else rectangle[1]
   
     # Here we are defining range of bluecolor in HSV
     # This creates a mask of blue coloured 
     # objects found in the frame.
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
   
     # The bitwise and of the frame and mask is done so 
     # that only the blue coloured objects are highlighted 
     # and stored in res
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+
     
-    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            cv2.circle(res, (i[0], i[1]), i[2], (0, 0, 0), 2)
+    bilateral_filtered_image = cv2.bilateralFilter(frame, 5, 120, 255)
+    edge_detected_image = cv2.Canny(bilateral_filtered_image, 50, 255)
+    keypoints = detector.detect(edge_detected_image)
+    blank = np.zeros((1, 1))
+    blobs = cv2.drawKeypoints(frame, keypoints, blank, (0, 0, 255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
 
     cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+    cv2.imshow('mask',edge_detected_image)
   
     # This displays the frame, mask 
     # and res which we created in 3 separate windows.
